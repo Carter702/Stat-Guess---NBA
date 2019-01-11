66a11953177954002b21ce2c0882ff43178f92a7
Khyri Carter <cartek3@unlv.nevada.edu>
2019-01-11 01:01:28 -0800

    various game card UI changes

--- app/src/main/java/com/carter/khyri/statguess_nba/adapters/GameAdapter.java
+++ app/src/main/java/com/carter/khyri/statguess_nba/adapters/GameAdapter.java
@@ -71,33 +71,100 @@ public class GameAdapter extends RecyclerView.Adapter<GameAdapter.GameViewHolder
 
         int resource = 0;
         switch (triCode) {
+            case "ATL":
+                resource = R.drawable.logo_atl;
+                break;
+            case "BKN":
+                resource = R.drawable.logo_bkn;
+                break;
             case "BOS":
                 resource = R.drawable.logo_bos;
                 break;
-            case "MIA":
-                resource = R.drawable.logo_mia;
+            case "CHA":
+                resource = R.drawable.logo_cha;
                 break;
-            case "LAC":
-                resource = R.drawable.logo_lac;
+            case "CHI":
+                resource = R.drawable.logo_chi;
+                break;
+            case "CLE":
+                resource = R.drawable.logo_cle;
+                break;
+            case "DAL":
+                resource = R.drawable.logo_dal;
                 break;
             case "DEN":
                 resource = R.drawable.logo_den;
                 break;
+            case "DET":
+                resource = R.drawable.logo_det;
+                break;
+            case "GSW":
+                resource = R.drawable.logo_gsw;
+                break;
+            case "HOU":
+                resource = R.drawable.logo_hou;
+                break;
+            case "IND":
+                resource = R.drawable.logo_ind;
+                break;
+            case "LAC":
+                resource = R.drawable.logo_lac;
+                break;
+            case "LAL":
+                resource = R.drawable.logo_las;
+                break;
+            case "MEM":
+                resource = R.drawable.logo_mem;
+                break;
+            case "MIA":
+                resource = R.drawable.logo_mia;
+                break;
+            case "MIL":
+                resource = R.drawable.logo_mil;
+                break;
+            case "MIN":
+                resource = R.drawable.logo_min;
+                break;
+            case "NOP":
+                resource = R.drawable.logo_nop;
+                break;
+            case "NYK":
+                resource = R.drawable.logo_nyk;
+                break;
             case "OKC":
                 resource = R.drawable.logo_okc;
                 break;
-            case "SAS":
-                resource = R.drawable.logo_sas;
+            case "ORL":
+                resource = R.drawable.logo_orl;
                 break;
-            case "DET":
-                resource = R.drawable.logo_det;
+            case "PHI":
+                resource = R.drawable.logo_phi;
+                break;
+            case "PHX":
+                resource = R.drawable.logo_phx;
+                break;
+            case "POR":
+                resource = R.drawable.logo_por;
                 break;
             case "SAC":
                 resource = R.drawable.logo_sac;
                 break;
+            case "SAS":
+                resource = R.drawable.logo_sas;
+                break;
+            case "TOR":
+                resource = R.drawable.logo_tor;
+                break;
+            case "UTA":
+                resource = R.drawable.logo_uta;
+                break;
+            case "WAS":
+                resource = R.drawable.logo_was;
+                break;
 
                 default:
                     resource = R.drawable.ic_basketball;
+                    break;
         }
 
         return resource;
