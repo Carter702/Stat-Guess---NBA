0f2d3db23d15c34c8e4fe301c491c0c69356c090
Khyri Carter <cartek3@unlv.nevada.edu>
2019-02-16 23:42:51 -0800

    removed retrofit call from DB

--- app/src/main/java/com/carter/khyri/statguess_nba/service/repository/AppDatabase.java
+++ app/src/main/java/com/carter/khyri/statguess_nba/service/repository/AppDatabase.java
@@ -25,55 +25,7 @@ public abstract class AppDatabase extends RoomDatabase {
     private static AppDatabase instance;
 
     public abstract TeamDao teamDao();
+
     public abstract PlayerDao playerDao();
-    private static Player player;
-    private static GameRepository gameRepo = new GameRepository();
 
-    public static synchronized AppDatabase getInstance(Context context) {
-        if (instance == null) {
-            instance = Room.databaseBuilder(context.getApplicationContext(),
-                    AppDatabase.class, "AppDatabase").fallbackToDestructiveMigration()
-                    .addCallback(roomCallback)
-                    .build();
-        }
-        return instance;
-    }
-
-    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
-        @Override
-        public void onCreate(@NonNull SupportSQLiteDatabase db) {
-            super.onCreate(db);
-
-//            gameRepo.getPlayers().observe(GlobalAppContext.getAppContext(), new Observer<Player>() {
-//                @Override
-//                public void onChanged(Player p) {
-//                    player = p;
-//                }
-//            });
-
-            new PopulateDbAsyncTask(instance).execute();
-        }
-    };
-
-    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
-        private PlayerDao playerDao;
-        private TeamDao teamDao;
-
-
-        private PopulateDbAsyncTask(AppDatabase db) {
-            playerDao = db.playerDao();
-            teamDao = db.teamDao();
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            List<Player.Person> players = player.getLeague().getPeople();
-
-            for (int i = 0; i < player.getLeague().getPeople().size(); i++) {
-                Player.Person person = players.get(i);
-                playerDao.insert(new Players(person.getPersonId(), person.getFirstName(), person.getLastName(), person.getTeamId(), person.getJersey()));
-            }
-            return null;
-        }
-    }
 }
